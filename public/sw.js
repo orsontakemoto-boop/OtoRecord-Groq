56b8b887e2c210dc951db55963a2d1cfafe808be
orsontakemoto-boop <orson.takemoto@gmail.com>
2025-12-17 18:09:52 -0300

    Ícone tambem no app

---
+++ public/sw.js
@@ -0,0 +1,24 @@
+
+const CACHE_NAME = 'otorecord-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/icon.svg',
+  '/manifest.json'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
